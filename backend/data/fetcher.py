0a4af7926088bd6cfa63a3ef0c7128322f7ec3c8
brian <brianyskeung@gmail.com>
2026-08-06 11:59:46 -0700

    Debugged adjusted close error in fetcher and added auto_adjust=True as failsafe

--- backend/data/fetcher.py
+++ backend/data/fetcher.py
@@ -5,23 +5,26 @@ def get_historical_returns(tickers: list[str], period: str = "5y") -> pd.DataFra
     """
     Downloads historical data and returns a DataFrame of daily percentage changes.
     """
-
     print(f"Fetching {period} of data for {tickers}...")
     
-    # download data 
-    data = yf.download(tickers, period=period, progress=False)
+    # download data (auto_adjust is True by default)
+    data = yf.download(tickers, period=period, progress=False, auto_adjust = True)
     
-    # extract Adjusted Close prices 
+    # safety check if the download fails
+    if data.empty:
+        raise ValueError("Failed to download market data.")
+    
+    # extract Adjusted Close prices
     if len(tickers) == 1:
-        adj_close = data[['Adj Close']].copy()
-        adj_close.columns = tickers
+        prices = data[['Close']].copy()
+        prices.columns = tickers
     else:
-        adj_close = data['Adj Close']
+        prices = data['Close']
         
     # sort columns into the order of the requested tickers
-    adj_close = adj_close[tickers]
+    prices = prices[tickers]
     
-    # calculate daily percentage returns and drop the first row (as no previous data availble)
-    daily_returns = adj_close.pct_change().dropna()
+    # calculate daily percentage returns and drop the first row
+    daily_returns = prices.pct_change().dropna()
     
     return daily_returns
